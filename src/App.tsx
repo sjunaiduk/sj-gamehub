8ecf0b739ca0553ca8f43b6a37f9baf2ccf56916
Syed Junaid <sjunaiduk@gmail.com>
2023-11-06 18:40:46 +0000

    Refactor layout using Chakra UI Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
   return (
-    <>
-      <Button colorScheme="red" size="lg">
-        Click Me
-      </Button>
-    </>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem gridArea="nav" h="10" bg="pink.500">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem gridArea="aside" h="10" bg="pink.500">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem gridArea="main" h="10" bg="pink.500">
+        Main
+      </GridItem>
+    </Grid>
   );
 }
 
