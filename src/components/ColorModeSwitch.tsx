889d5a97841bb0029c2cf53e283cc62f8562ea4f
Syed Junaid <sjunaiduk@gmail.com>
2023-11-10 00:31:22 +0000

    changed dark/light mode toggle to button.

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,28 +1,13 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { Button, HStack, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
-  const [toggle, setToggle] = useState(false);
-
-  useEffect(() => {
-    const timeOut = setTimeout(() => {
-      toggleColorMode();
-    }, 25);
-
-    return () => clearTimeout(timeOut);
-  }, [toggle]);
 
   return (
     <HStack marginRight={1}>
-      <Switch
-        // isChecked={colorMode === "dark"}
-        onChange={() => setToggle(!toggle)}
-        colorScheme="green"
-      />
-      <Text whiteSpace="nowrap">
-        Toggle {colorMode === "light" ? "Dark" : "Light"}
-      </Text>
+      <Button onClick={toggleColorMode} size="md" colorScheme="gray">
+        {colorMode === "light" ? "Dark" : "Light"}
+      </Button>
     </HStack>
   );
 };
