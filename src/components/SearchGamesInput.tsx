5c79b6d05636403dd10840e181b353c0cb3a7c8b
unknown <sjunaiduk@gmail.com>
2023-12-15 21:03:40 +0000

    redirect to homepage on search

--- src/components/SearchGamesInput.tsx
+++ src/components/SearchGamesInput.tsx
@@ -2,16 +2,22 @@ import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
-
+import { useLocation, useNavigate, useNavigation } from "react-router-dom";
 
 const SearchGamesInput = () => {
   const ref = useRef<HTMLInputElement>(null);
+  const nav = useNavigate();
+  const loc = useLocation();
   const store = useGameQueryStore();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
         store.setSearchText(ref.current?.value || "");
+        console.log(loc);
+        if (loc.pathname != "/") {
+          nav("/");
+        }
       }}
     >
       <InputGroup>
